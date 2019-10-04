6ea8df8e99cff9b591b1b765a62b5866b0cb6f77
jvyden420 <jvyden420@gmail.com>
2019-10-04 10:59:39 -0400

    now watch me whip

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -3,13 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  base_api_url: '/api/',
-  osu_redirect_url: 'http://localhost:4200/api/login-verify',
-  osu_oauth_id: '46',
-  twitch_redirect_url: 'http://localhost:4200/api/twitch-redirect',
-  twitch_client_id: 'a27rae8bkv7ypqtponiypw15wwzdy0',
-  socket_url: 'http://localhost:4100',
+  production: true,
+  base_api_url: 'https://play.osustuff.org/api/',
+  osu_redirect_url: 'https://play.osustuff.org/api/login-verify',
+  osu_oauth_id: '48',
+  twitch_redirect_url: 'https://play.osustuff.org/api/twitch-redirect',
+  twitch_client_id: 'le90ws5nsckurdggyl8geafm3uxb8w',
+  socket_url: 'https://play.osustuff.org:2053',
 };
 
 /*
